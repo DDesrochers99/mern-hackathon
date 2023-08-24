6859279833d3a411748045d723e78278bf200654
Jay <jay@wjdonaldson.com>
2023-08-24 10:37:46 -0700

    create OrderHisterPage starter

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,7 +1,26 @@
-export default function OrderHistoryPage() {
+import { useState, useEffect } from 'react';
+import * as ordersAPI from '../../utilities/orders-api';
+import './OrderHistoryPage.css';
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function OrderHistoryPage({ user, setUser }) {
+
+  useEffect(function() {
+  }, []);
+
   return (
-    <>
-      <h1>OrderHistoryPage</h1>
-    </>
+    <main className="OrderHistoryPage">
+      <aside>
+        <Logo />
+        <Link to="/" className="button btn-sm">NEW ORDER</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <OrderDetail
+        order={null}
+      />
+    </main>
   );
 }
