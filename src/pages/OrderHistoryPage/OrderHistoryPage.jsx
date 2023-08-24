d0d31552da22578f2fa2dff554e1b98853b9644f
ddesrochers99 <desrochersdale@gmail.com>
2023-08-24 11:54:35 -0700

    adds jsx to OrderHistoryPage

--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -5,22 +5,36 @@ import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../components/Logo/Logo';
 import OrderDetail from '../../components/OrderDetail/OrderDetail';
 import UserLogOut from '../../components/UserLogOut/UserLogOut';
+import OrderList from "../../components/OrderList/OrderList";
 
 export default function OrderHistoryPage({ user, setUser }) {
+  const [orders, setOrders] = useState([]);
+  const [currentOrder, setCurrentOrder] = useState(null);
 
-  useEffect(function() {
+  useEffect(function () {
+    async function getOrders() {
+      const orders = await ordersAPI.getOrders();
+      setCurrentOrder(orders[0] || null);
+      setOrders(orders);
+    }
+    getOrders();
   }, []);
 
   return (
     <main className="OrderHistoryPage">
       <aside>
         <Logo />
-        <Link to="/" className="button btn-sm">NEW ORDER</Link>
+        <Link to="/orders/new" className="button btn-sm">
+          NEW ORDER
+        </Link>
         <UserLogOut user={user} setUser={setUser} />
       </aside>
-      <OrderDetail
-        order={null}
+      <OrderList
+        orders={orders}
+        currentOrder={currentOrder}
+        setCurrentOrder={setCurrentOrder}
       />
+      <OrderDetail order={currentOrder} />
     </main>
   );
 }
