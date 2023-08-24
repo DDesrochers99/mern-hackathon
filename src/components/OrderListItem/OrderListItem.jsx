7939e109477edbb87e81fa59c96ed9267cb9e730
ddesrochers99 <desrochersdale@gmail.com>
2023-08-24 11:30:58 -0700

    fixes typos

--- src/components/OrderListItem/OrderListItem.jsx
+++ src/components/OrderListItem/OrderListItem.jsx
@@ -8,15 +8,15 @@ export default function OrderListItem({ order, currentOrder, setCurrentOrder }) 
     >
       <div>
         <div>
-          Order Id: <span className="smaller">{order.orderId}</span>
+          Order Id: <span className="">{order.orderId}</span>
         </div>
-        <div className="smaller">
+        <div className="">
           {new Date(order.updatedAt).toLocaleDateString()}
         </div>
       </div>
-      <div className="align-rt">
+      <div className="">
         <div>${order.orderTotal.toFixed(2)}</div>
-        <div className="smaller">
+        <div className="">
           {order.orderQty} Item{order.orderQty > 1 ? "s" : ""}
         </div>
       </div>
